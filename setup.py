5dd7a7e8ef1697641b5898677d367864050982a3
Your Name <you@example.com>
2024-09-25 12:26:26 -0500

    added appdirs req.

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     packages=find_packages(where='src'),
     install_requires=[
         'setuptools',
-        'pygame-ce'
+        'pygame-ce',
+        'appdirs'
     ],
     package_dir={'': 'src'},
     include_package_data=True,
